b16afec6f2411da251e3d711980e5261e558e60b
Volodymyr Dzhuryn <volodymyr.dzhuryn@otakoyi.com>
2024-09-27 12:22:38 +0300

    refactor: remove wrong comment

--- src/Kernel.php
+++ src/Kernel.php
@@ -5,7 +5,6 @@ namespace App;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
-// test
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
