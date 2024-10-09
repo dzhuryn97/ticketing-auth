e6f37ef398ecdc8d2891ee8a95d051c939a75b9d
Volodymyr Dzhuryn <volodymyr.dzhuryn@otakoyi.com>
2024-10-09 19:52:33 +0300

    fix env config for fixture bundle

--- config/bundles.php
+++ config/bundles.php
@@ -12,5 +12,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
 ];
